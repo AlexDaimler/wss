a92ca4d90771592eaa2fb62ee4badea0e23a794e
Alex <Alex@DESKTOP-DUG97TK.mshome.net>
2020-09-26 12:13:31 +0800

    Change Test folder

--- src/test/java/com/bestseller/wss/WssApplicationTests.java
+++ src/test/java/com/bestseller/wss/WssApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.bestseller.wss;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class WssApplicationTests {
 
 	@Test
 	void contextLoads() {
